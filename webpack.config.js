57b3a3c7f8937f74392836253d96e60ff457223f
Javier <javidx@gmail.com>
2023-01-31 15:25:15 +0200

    Added setup for webpack, bootstrap and config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins() {
+                return [
+                  require('autoprefixer'),
+                ];
+              },
+            },
+          },
+        }, {
+          loader: 'sass-loader',
+        }], 
+      },{
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|tests)/,
+        use: ['babel-loader']
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leaderboard',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
